1b9465b2ceae881620557266373e8c56f876950d
Peter Širka <petersirka@gmail.com>
2023-10-10 14:31:57 +0200

    Added support for external Flow clients.

--- controllers/default.js
+++ controllers/default.js
@@ -1,5 +1,6 @@
 exports.install = function() {
 	ROUTE('GET /', index);
+	ROUTE('SOCKET /{id}/', socket, 1024 * 10);
 };
 
 function index() {
@@ -8,4 +9,31 @@ function index() {
 		$.view('index');
 	else
 		$.redirect('https://floweditor.totaljs.com');
+}
+
+function socket() {
+
+	var $ = this;
+	var flow = null;
+
+	$.autodestroy();
+
+	$.on('open', function(client) {
+		if (client.query.flow || client.query.flowstream) {
+			flow = client;
+			if ($.online > 1)
+				flow.send({ TYPE: 'flow' });
+		} else if (!flow)
+			client.close(4004);
+	});
+
+	$.on('close', function(client) {
+		if (client.query.flow || client.query.flowstream)
+			flow = null;
+	});
+
+	$.on('message', function(client, msg) {
+		$.send(msg, c => c !== client);
+	});
+
 }
